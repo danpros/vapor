d80916829874143a0258f770d1c964096f34832c
Dan <dan@danpros.com>
2024-03-15 16:03:58 +0700

    Update layout.html.php

--- layout.html.php
+++ layout.html.php
@@ -6,6 +6,7 @@
     <title><?php echo $title;?></title>
     <meta name="description" content="<?php echo $description; ?>"/>
     <link rel="canonical" href="<?php echo $canonical; ?>" />
+    <?php echo $metatags;?>
     <link rel="stylesheet" type="text/css" href="<?php echo theme_path();?>css/normalize.css">
     <link rel="stylesheet" type="text/css" href="<?php echo theme_path();?>css/screen.css">
     <link rel="stylesheet" type="text/css" href="<?php echo theme_path();?>css/font-awesome.min.css">
